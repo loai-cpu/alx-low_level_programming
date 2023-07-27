7c79439a2b0a467e17a0a65f2adfe40f97894bbc
me <you@example.com>
2023-07-27 08:44:29 -0700

    2 modei

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,32 @@
 #include "main.h"
 /**
- * _strncat - concatenates at most n bytes from src to dest
- * @dest: destination string
- * @src: source string
- * @n: number of bytes to copy
- *
- * Return: pointer to the destination string
+ *_strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
  */
+
 char *_strncat(char *dest, char *src, int n)
+
 {
 int i, j;
 
-for (i = 0; dest[i] != '\0'; i++)
-    ;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
 
-for (j = 0; j < n && src[j] != '\0'; j++)
-dest[i + j] = src[j];
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
 
-dest[i + j] = '\0';
+dest[i] = '\0';
 
 return (dest);
 }
