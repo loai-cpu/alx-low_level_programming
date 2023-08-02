3dadcd53440a7ef3d8e5f18a4a8952ccd1dc3166
me <you@example.com>
2023-08-02 05:16:23 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string recursively
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	printf("%c", *s);
+	_puts_recursion((s + 1));
+}
